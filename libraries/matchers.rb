2bae2577bce9bb7872ebb5a43c89b322c3a4c1f7
Julien Berard <jujugrrr@gmail.com>
2016-01-13 15:54:32 +0000

    Added a matcher for the default library

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,6 +1,10 @@
 if defined?(ChefSpec)
   ChefSpec.define_matcher :newrelic_meetme_plugin_cfg
 
+  def install_newrelic_meetme_plugin(name)
+    ChefSpec::Matchers::ResourceMatcher.new(:newrelic_meetme_plugin, :install, name)
+  end
+
   def generate_newrelic_meetme_plugin_cfg(config_file)
     ChefSpec::Matchers::ResourceMatcher.new(:newrelic_meetme_plugin_cfg, :generate, config_file)
   end
